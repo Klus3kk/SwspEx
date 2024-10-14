feddc5d653747182534eacc8a4334b4e7c3e8873
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-10-14 09:22:29 +0200

    Updated procesy.c.

--- procesy.c
+++ procesy.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
-int main(){
-    printf ("Begin"); // Difference with '\n' - flush, wymusza
-    fork();
-    printf ("End");
+int main() {
+    if(fork() == 0) { // If 0, then it's potomna sekcja
+        // printf("Child\n");
+        printf("--------\n");
+        printf("PID: %d\n", getpid());
+        printf("PPID: %d\n", getppid());
+        printf("--------\n");
+    } else {
+        // printf("Parent\n");
+        printf("--------\n");
+        printf("PPID: %d\n", getppid());
+        printf("PID: %d\n", getpid());
+        printf("--------\n");
+    }
+
+    // printf ("Begin"); // Difference with '\n' - flush, wymusza
+    // fork();
+    // printf ("End");
     return 0;
 }
+
