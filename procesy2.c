179e2e5e1327b6b46ac925ee10bf2ab061a3f9c3
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-10-21 08:49:09 +0200

    Process problems in ASCI C.

--- procesy2.c
+++ procesy2.c
@@ -6,7 +6,9 @@ int main(){ // We can't say which process will be first
     fork();
     printf("Ha\n");
     fork();
+    wait(NULL); // Pochodne procesy czekaja
     printf("Ho\n");
     exit(7); // We can also use kill(pid, signum)
+    
     return 0;
 }
---
+++ procesy3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+
+int main(int argc, char* argv[]){
+    int status = 4;
+    if(fork() != 0) {
+        wait(&status);
+    } else {
+        exit(1);
+        // kill(1,&status);
+    }
+    printf("%x\n", status);
+    printf("%04x\n", status);
+
+    // exit(), pozniej kill()
+    return 0;
+}
