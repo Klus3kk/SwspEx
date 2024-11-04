1c1ef61d19bab6cc482a309233455781405eb3ef
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-11-04 09:15:49 +0100

    Made Zadanie4.c in "Potoki nienazwane".

---
+++ Potoki nienazwane/zadanie4.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    int fd1[2], fd2[2], fd3[2];
+    int numbers[4] = {1, 2, 3, 4};
+    
+    if (pipe(fd1) == -1 || pipe(fd2) == -1 || pipe(fd3) == -1) {
+        perror("pipe");
+        return 1;
+    }
+
+    pid_t pid1 = fork();
+    if (pid1 == 0) {
+        close(fd1[0]);
+        write(fd1[1], numbers, sizeof(numbers));
+        close(fd1[1]);
+        return 0;
+    }
+
+    pid_t pid2 = fork();
+    if (pid2 == 0) {
+        close(fd1[1]);
+        close(fd2[0]);
+        read(fd1[0], numbers, sizeof(numbers));
+        for (int i = 0; i < 4; i++) {
+            numbers[i] += 1;
+        }
+        write(fd2[1], numbers, sizeof(numbers));
+        close(fd1[0]);
+        close(fd2[1]);
+        return 0;
+    }
+
+    pid_t pid3 = fork();
+    if (pid3 == 0) {
+        close(fd2[1]);
+        close(fd3[0]);
+        read(fd2[0], numbers, sizeof(numbers));
+        for (int i = 0; i < 4; i++) {
+            numbers[i] += 1;
+        }
+        write(fd3[1], numbers, sizeof(numbers));
+        close(fd2[0]);
+        close(fd3[1]);
+        return 0;
+    }
+
+    pid_t pid4 = fork();
+    if (pid4 == 0) {
+        close(fd3[1]);
+        read(fd3[0], numbers, sizeof(numbers));
+        for (int i = 0; i < 4; i++) {
+            numbers[i] += 1;
+        }
+        printf("Ostateczne liczby: ");
+        for (int i = 0; i < 4; i++) {
+            printf("%d ", numbers[i]);
+        }
+        printf("\n");
+        close(fd3[0]);
+        return 0;
+    }
+
+    close(fd1[0]);
+    close(fd1[1]);
+    close(fd2[0]);
+    close(fd2[1]);
+    close(fd3[0]);
+    close(fd3[1]);
+
+    return 0;
+}
